a0e2d7d28f23528fa55b3a7d0a291f26ac609279
Bartłomiej Juda <bartekjkk@gmail.com>
2021-07-23 19:10:39 +0700

    some changes

---
+++ pythON poczatek moj/prog objektowe zad1.py
@@ -0,0 +1,16 @@
+# Utwórz klasy do reprezentacji Produktu, Zamówienia, Jabłek i Ziemniaków.
+#
+# Stwórz po kilka obiektów typu jabłko i ziemniak i wypisz ich typ za pomocą funkcji wbudowanej type.
+#
+# Stwórz listę zawierającą 5 zamówień oraz słownik, w którym kluczami będą nazwy
+# produktów, a wartościami instancje klasy produkt.
+
+class Produkt:
+    pass
+class Zamówienie:
+    pass
+class Jabłko:
+    pass
+class Ziemiak:
+    pass
+if __name__=='__main__':
