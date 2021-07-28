9f35835245f9e06d0534a9c394fde233d6c08630
Bartłomiej Juda <bartekjkk@gmail.com>
2021-07-28 20:09:17 +0700

    some changes

--- pythON poczatek moj/programowanie objektowe/shop/class_order.py
+++ pythON poczatek moj/programowanie objektowe/shop/class_order.py
@@ -1,18 +1,18 @@
 import random
-from shop.class_Order_element import OrderElement
+from shop.class_product import Product
 
 
 class Order:
-    def __init__(self,orderer_first_name, orderer_last_name,positions_list=None):
-        if positions_list is None:
-            positions_list = []
-        self.positions_list = positions_list
+    def __init__(self,orderer_first_name, orderer_last_name,products_list=None):
+        if products_list is None:
+            products_list = []
+        self.positions_list = products_list
         self.orderer_last_name = orderer_last_name
         self.orderer_first_name = orderer_first_name
-        # total_prize = 0
-        # for product in positions_list:
-        #     total_prize += product.prize
-        # self.total_prize = total_prize
+        total_prize = 0
+        for product in products_list:
+            total_prize += product.prize
+        self.total_prize = total_prize
 
 
 
@@ -30,10 +30,10 @@ def generate_an_order():
     number_of_products= random.randint(1,20)
     products= []
     for product_number in range (number_of_products):
-        product_name= f"Product- {product_number}"
+        product_name = f"Product- {product_number}"
         product_category = "inne"
         product_prize = random.randint(1,30)
-        product = OrderElement (product,)
+        product = Product (product_name,product_category,product_prize)
         products.append(product)
     order = Order("Bartłomiej", "Juda",products)
     return order
