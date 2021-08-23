f950773f0696285ef36dcb80a02b2488a8e387d7
Bartłomiej Juda <bartekjkk@gmail.com>
2021-08-23 13:53:00 +0700

    some changes

--- pythON poczatek moj/programowanie objektowe/shop/class_product.py
+++ pythON poczatek moj/programowanie objektowe/shop/class_product.py
@@ -1,9 +1,4 @@
-# Stwórz klasę reprezentującą produkt z terminem ważności (dziedziczącą po klasie Product).
-#
-# Rozszerz implementację bazową o dodatkowe pola: rok produkcji oraz liczbę lat ważności i metodę does_expire.
-#
-# Metoda ta przyjmuje jako argument aktualny rok oraz sprawdza czy od daty produkcji do podanego roku minęła
-# liczba lat większa od tej podanej jako lata ważności
+
 
 
 class  Product:
