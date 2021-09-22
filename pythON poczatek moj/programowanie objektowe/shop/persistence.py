6fcf56da28ec99be052ad6785ff3f40b187abbf3
Bartłomiej Juda <bartekjkk@gmail.com>
2021-09-22 13:16:53 +0700

    some changes

--- pythON poczatek moj/programowanie objektowe/shop/persistence.py
+++ pythON poczatek moj/programowanie objektowe/shop/persistence.py
@@ -5,28 +5,37 @@ from shop.store import AvailableProduct,ProductCategory
 
 def update_available_products(available_products, file_name="available_products.csv"):
     with open (file_name, mode="w",newline="",encoding= 'utf-8') as available_products_file:
-        csv_writer = csv.writer(available_products_file)
-        csv_writer.writerow(["name","category","unit_price","identifier","quantity"])
+        headers = ["name", "category", "unit_price", "identifier", "quantity"]
+        csv_writer = csv.DictWriter(available_products_file,fieldnames=headers)
+        csv_writer.writeheader()
+
         for  available_product in available_products:
             product = available_product.product
-            csv_writer.writerow([product.name, product.category.name, product.price, product.identifier, available_product.quantity]
+            csv_writer.writerow({
+                "name":product.name,
+                "category":product.category.name,
+                "unit_price":product.price,
+                "identifier":product.identifier,
+                "quantity":available_product.quantity}
                                 )
 
 
 
 def load_available_products(file_name="available_products.csv"):
     with open(file_name,newline="",encoding="utf-8")as available_file:
-        csv_reader= csv.reader(available_file)
-        next(csv_reader)
-        return [ AvailableProduct(
-            name =row[0],
-            category=ProductCategory[row[1]],
-            unit_price=float(row[2]),
-            identifier=int(row[3]),
-            quantity=int(row[4]),
+        csv_reader= csv.DictReader(available_file)
+        return [
+            AvailableProduct(
+                name =row["name"],
+                category=ProductCategory[row["category"]],
+                unit_price=float(row["unit_price"]),
+                identifier=int(row["identifier"]),
+                quantity=int(row["quantity"]),
             )
             for row in csv_reader
         ]
+
+
 
 
 def save_order(order):
