66bb6c4be0e0c38a466c4fb5a829fbfaa600ffe8
Bartłomiej Juda <bartekjkk@gmail.com>
2021-10-14 15:34:27 +0700

    some changes

--- exercises/cows and bulls game.py
+++ exercises/cows and bulls game.py
@@ -3,6 +3,7 @@ import random
 def welcome():
     name= input("What's your name? >>>")
     print(f"{name} Welcome to the Cows and Bulls game!")
+    return name
 
 def generate_secret_number():
     secret_number = ""
@@ -15,8 +16,9 @@ def game (secret_number, name):
     number_of_tries= 0
     while True:
         number_of_tries +=1
-        guess = input(str(""" Enter 4-digits number
->>>"""))
+        guess = input(
+""" Enter 4-digits number
+>>>""")
         number_of_cows= 0
         number_of_bulls=0
         if guess == secret_number:
@@ -26,19 +28,32 @@ def game (secret_number, name):
             else:
                 print(f"Conratulation {name}, your answer correctly in {number_of_tries} tries! the number it's {secret_number}")
                 break
-        for element in (guess):
-            if element == element in secret_number:
-                # number_of_cows +=1
-            # elif element in secret_number:
-                number_of_bulls +=1
+        for index,number in enumerate(guess):
+            a= number
+            a_index = index
+            if a in secret_number:
+                for index, number in enumerate(secret_number):
+                    b= number
+                    b_index= index
+                    if a==b and  a_index == b_index:
+                        number_of_cows +=1
+                        continue
+                    elif a==b and a_index != b_index:
+                        number_of_bulls +=1
+                        continue
+
 
         print(f"cows:{number_of_cows}, bulls: {number_of_bulls}")
 
 def run_game():
     name= welcome()
     secret_number= generate_secret_number()
-    print(secret_number)
     game(secret_number,name)
+
+
+
+
+
 
 if __name__=="__main__":
     run_game()
