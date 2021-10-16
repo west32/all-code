edbccc5029d5ac2fbcb0a14a781070781dc32a8c
Bartłomiej Juda <bartekjkk@gmail.com>
2021-10-16 20:20:55 +0700

    some changes

--- exercises/19.py
+++ exercises/19.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import openpyxl
+
 
 url_page="https://www.nytimes.com/"
 
@@ -14,7 +14,7 @@ titles_list = []
 title_tags= parsed_page.find_all(is_it_title_tag)
 for title in title_tags:
     titles_list.append(title.string)
-# print(titles_list)
+
 
 
 #
@@ -22,7 +22,3 @@ file_name=input("what should be name of that file?")
 with open(f"{file_name}.txt","w",encoding='utf-8') as open_file:
     for article in titles_list:
         open_file.write (f"{str(article)}\n")
-
-
-        # open_file.write(article.text.replace(", ", "\n ").strip())
-
