091c423071be28a5d2ecf8dceda270184a3bd7c4
Bartłomiej Juda <bartekjkk@gmail.com>
2021-11-04 12:32:00 +0700

    some changes

--- MOSH Course/weight_excel.py
+++ MOSH Course/weight_excel.py
@@ -1,33 +1,47 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference,LineChart
+from openpyxl import Workbook
 from datetime import date
 from openpyxl.chart.axis import DateAxis
 
 # cll = sheet['a1']
 # cllone=sheet.cll(1,1)
-work_book=xl.load_workbook("weight2.xlsx")
-sheet = work_book['Sheet1']
+workbook=xl.load_workbook("weight3.xlsx")
+sheet = workbook['Sheet1']
+ws= workbook.active
 
-
-
+#
 date_cell = sheet.cell(sheet.max_row + 1, 1)
 weight_cell = sheet.cell(sheet.max_row, 2)
-today_date=input("eneter today date:")
-today_weight= float(input("eneter today weight"))
+today_date=input("eneter today's date:")
+today_weight= float(input("eneter today's weight"))
 date_cell.value=today_date
 weight_cell.value=today_weight
 
-weights_and_dates= Reference(sheet,
-          min_row=2,
-          max_row=sheet.max_row,
-          min_col=2,
-          max_col=2)
+weights= Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=2,
+                   max_col=2)
 
 chart = LineChart()
-chart.title= "Weight drop"
-chart.add_data(weights_and_dates)
+chart.title= "Weight loss"
+chart.style= 2
+chart.y_axis.title="Weight"
+chart.y_axis.crossAx= 500
+chart.x_axis= DateAxis(crossAx=100)
+chart.x_axis.number_format = 'd=mmm'
+chart.x_axis.majorTimeUnit= "days"
+chart.x_axis.title = "Date"
+# chart.add_data(weights)
+chart.add_data(weights)
+dates= Reference(sheet,min_col=1,min_row=2,max_row=sheet.max_row)
+chart.set_categories(dates)
+
+
+
 sheet.add_chart(chart,'d2')
 
 
 
-work_book.save("weight3.xlsx")
+workbook.save("weight3.xlsx")
