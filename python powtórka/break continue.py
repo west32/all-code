0922bf8abef670792bc1fe5d7d43eef581a72b9b
Bartłomiej Juda <bartekjkk@gmail.com>
2022-01-25 14:30:29 +0100

    some changes

--- python powtórka/break continue.py
+++ python powtórka/break continue.py
@@ -18,59 +18,59 @@ from random import sample, randint
 
 
 # 2
-#
-#
-# expenditures= {}
-#
-# category=""
-#
-# price=""
-#
-# while True:
-#     category = input("wprowadz kategorie wydatków, 'x' aby zakończyć ").lower()
-#     if category == "x":
-#         break
-#
-#         expenditures[category]={}
-#         expenditure = ""
-#         while True:
-#                 expenditure = input(f"co kupiłeś z kategorii {category}? 'x' aby dodać kolejną kategorię ").lower()
-#                 if expenditure == "x":
-#                     break
-#
-#                 price= input(f"ile wydałeś na {expenditure}?")
-#                 expenditures[category][expenditure]=float(price)
-#                 print(expenditures)
-#
-#
-# all_cost=0
-# most_expensive_cat=None
-# bigest_udzial = 0
-# for  values  in expenditures.values():
-#     for expenditure in values.values():
-#         all_cost += expenditure
-#
-# for categories, values in expenditures.items():
-#     for expenditure in values.values():
-#
-#         udzial_kategorii= float(sum(values.values())/all_cost*100)
-#         print(f"Udział kategorii {categories} to {udzial_kategorii:.2f}%")
-#         if udzial_kategorii > bigest_udzial:
-#             bigest_udzial = udzial_kategorii
-#             most_expensive_cat = categories
-#
-# for categories, dicts in expenditures.items():
-#     print(f"{categories}- {sum(dicts.values())}")
-#
-# print(f"kategoria z największym udziałem to {most_expensive_cat} - {bigest_udzial:.2f}% wszystkich wydatków")
+
+
+expenditures= {}
+
+category=""
+
+price=""
+
+while True:
+    category = input("wprowadz kategorie wydatków, 'x' aby zakończyć ").lower()
+    if category == "x":
+        break
+
+    expenditures[category]={}
+    expenditure = ""
+    while True:
+            expenditure = input(f"co kupiłeś z kategorii {category}? 'x' aby dodać kolejną kategorię ").lower()
+            if expenditure == "x":
+                break
+
+            price= input(f"ile wydałeś na {expenditure}?")
+            expenditures[category][expenditure]=float(price)
+            print(expenditures)
+
+
+all_cost=0
+most_expensive_cat=None
+bigest_udzial = 0
+for  values  in expenditures.values():
+    for expenditure in values.values():
+        all_cost += expenditure
+
+for categories, values in expenditures.items():
+    for expenditure in values.values():
+
+        udzial_kategorii= float(sum(values.values())/all_cost*100)
+        print(f"Udział kategorii {categories} to {udzial_kategorii:.2f}%")
+        if udzial_kategorii > bigest_udzial:
+            bigest_udzial = udzial_kategorii
+            most_expensive_cat = categories
+
+for categories, dicts in expenditures.items():
+    print(f"{categories}- {sum(dicts.values())}")
+
+print(f"kategoria z największym udziałem to {most_expensive_cat} - {bigest_udzial:.2f}% wszystkich wydatków")
 
 
 # 3
-
-random_numbers = random.sample(range(100),random.randint(1,20))
-print(random_numbers)
-
-for number in random_numbers:
-    if number %2==0:
-        continue
-    print(number)
+#
+# random_numbers = random.sample(range(100),random.randint(1,20))
+# print(random_numbers)
+#
+# for number in random_numbers:
+#     if number %2==0:
+#         continue
+#     print(number)
--- python powtórka/funkcje 1.py
+++ python powtórka/funkcje 1.py
@@ -1,12 +1,33 @@
-def pole_prostokąta(a,b):
-    pole= a * b
-    return pole
+# def pole_prostokąta(a,b):
+#     pole= a * b
+#     return pole
+#
+# def run_example():
+#     bok_a = int(input("podaj pole boku a "))
+#     bok_b = int(input("podaj pole boku b "))
+#
+#     print(pole_prostokąta(bok_a,bok_b))
+#
+# if __name__=="__main__":
+#     run_example()
 
-def run_example():
-    bok_a = int(input("podaj pole boku a "))
-    bok_b = int(input("podaj pole boku b "))
+# 2
 
-    print(pole_prostokąta(bok_a,bok_b))
+# Napisz funkcję liczącą prędkość średnią na podstawie zadanego dystansu i czasu.
+#
+# Następnie wykorzystaj ją implementując program, który wyznaczy:
+#
+# średnią prędkość biegu
+# średnią prędkość jazdy na rowerze
+# średnią prędkość jazdy autem
+
+def avg_speed(distance,time):
+    return distance/time
+
+def run_homework():
+    run_distance= float(input("jaki dystans przebiegłęs (w km)? "))
+    run_time= float(input("ile Ci to zajeło (w h)? "))
+    print(f"Twoja średnia prędkość biegu wynosiłą {avg_speed(run_distance,run_time):.2f} km/h")
 
 if __name__=="__main__":
-    run_example()
+    run_homework()
